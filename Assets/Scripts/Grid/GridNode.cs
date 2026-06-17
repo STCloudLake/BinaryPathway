bade1bfa2989518762fd1394a9826a79ddc3d83d
CloudLake <2330275926@qq.com>
2026-06-17 14:25:12 +0800

    feat: add CellState field to GridNode

--- Assets/Scripts/Grid/GridNode.cs
+++ Assets/Scripts/Grid/GridNode.cs
@@ -8,11 +8,15 @@ public class GridNode
 	public bool occupied;
 	public TileBase placedTile;
 
+	/// <summary>Logical state of this grid cell. Defaults to PureValue(0) when empty.</summary>
+	public CellState cellState;
+
 	public GridNode(GridIndex idx, Vector3 pos)
 	{
 		index = idx;
 		worldPos = pos;
 		occupied = false;
 		placedTile = null;
+		cellState = CellState.PureValue(0);
 	}
 }
